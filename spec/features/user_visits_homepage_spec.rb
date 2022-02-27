b2d4458cc6cd8ee37bcdc1e9bd598fdb2e8876f1
cantstopcoding <jonathan_koss@yahoo.com>
2022-02-27 17:41:01 -0500

    write visit, expect and have_css methods

--- spec/features/user_visits_homepage_spec.rb
+++ spec/features/user_visits_homepage_spec.rb
@@ -2,5 +2,8 @@ require 'rails_helper'
 
 feature 'User visits homepage' do
   scenario 'and finds header with specific text' do
+    visit root_path
+
+    expect(page).to(have_css('h1', text: 'Task Creator'))
   end
 end
